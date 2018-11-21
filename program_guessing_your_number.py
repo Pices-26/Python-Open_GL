c00f7dd85911765f2c3e0e89b005ce3d1f138d3f
Marcel Kowalczyk <44699834+Pices-26@users.noreply.github.com>
2018-11-21 10:38:47 +0000

    Add files via upload

--- program_guessing_your_number.py
+++ program_guessing_your_number.py
@@ -1,31 +1,45 @@
-#not the most efficient as it does not close down on range
-#work in progress
 import random
-def guess(machine, usr):
-    if usr == 'l':
-        guess_machine = random.randrange(machine)
-        return(guess_machine)
-    else:
-        guess_machine = random.randrange(machine, 10)
-        return(guess_machine)
+#guessing function
+def guess(machine_max,machine_min):
+        machine = random.randrange(machine_min,machine_max)
+        return(machine)
+#defininf all variables and initial guess
 counter = 0
 print('hello, think of a number between 1 and 10 and I will guess it')
-guess_machine = random.randrange(10)
-print('my first guess is ' + str(guess_machine))
+machine = random.randrange(10)
+print('my first guess is ' + str(machine))
+print('if this is your nr then enter Y')
+print('if your nr is lower enter L')
+print('if your nr is higher enter H')
 usr = ''
+machine_min = 0
+machine_max = 10
+#guessing loop
 while usr != 'y':
-    print('if this is your nr then enter y')
-    print('if your nr is lower enter l')
-    print('if your nr is higher enter h')
+    usr = input('please enter your input: ')
 
-    usr = input('please enter your input')
-
-    if usr == 'y':
+    if usr == 'Y':
         print('great, I did it')
         break
-    else:
-        new_guess =guess(guess_machine, usr)
-        print(new_guess)
-    counter +=1
+    if usr == 'H':
+        machine_min = machine
+        if machine_max == 10:
+            machine_max = 10
+        else:
+            machine_max = machine_max
+
+        machine =guess(machine_max,machine_min)
+        print(machine)
+        counter +=1
+    if usr =='L':
+        machine_max = machine
+        if machine_min == 0:
+            machine_min = 0
+        else:
+            machine_min = machine_min
+
+        machine =guess(machine_max, machine_min)
+        print(machine)
+        counter +=1
 
 print('it took ' + str(counter)+ ' guesses to guess the number')
